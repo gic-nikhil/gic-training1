4ce7a0834594e39ec92b951cf802cc1c329bb93d
Nikhil kumar <nikhil.kumar@gridinfocom.com>
2021-09-07 12:15:08 +0530

    first commit

---
+++ src/Scope.java
@@ -0,0 +1,18 @@
+
+public class Scope {
+	public static void main(String args[])
+    {
+        {
+            // The variable x has scope within
+            // brackets
+            int x = 10;
+            System.out.println(x);
+        }
+         
+        // Uncommenting below line would produce
+        // error since variable x is out of scope.
+ 
+        // System.out.println(x);
+    }
+}
+
