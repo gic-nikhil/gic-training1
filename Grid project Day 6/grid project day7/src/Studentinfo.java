904c7aadcd465113b0f74e0ac2fd02b036ec77fd
Nikhil kumar <nikhil.kumar@gridinfocom.com>
2021-09-14 15:48:00 +0530

    third commit

---
+++ Grid project Day 6/grid project day7/src/Studentinfo.java
@@ -0,0 +1,36 @@
+import java.io.*;
+public class Studentinfo implements Serializable
+{
+    String name;
+    int rid;
+    static String contact;
+    Studentinfo(String n, int r, String c)
+    {
+    this.name = n;
+    this.rid = r;
+    this.contact = c;
+    }
+}
+
+class Demo
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Studentinfo si = new Studentinfo("Abhi", 104, "110044");
+            FileOutputStream fos = new FileOutputStream("student.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(si);
+            oos.flush();
+            oos.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
+
+
+
