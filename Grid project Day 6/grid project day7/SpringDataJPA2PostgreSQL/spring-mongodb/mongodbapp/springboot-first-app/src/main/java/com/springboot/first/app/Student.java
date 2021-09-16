ee65f808f6362b1e99f96c5cc1b7eb437ee89a11
Nikhil kumar <nikhil.kumar@gridinfocom.com>
2021-09-16 15:21:07 +0530

    fifth commit

---
+++ Grid project Day 6/grid project day7/SpringDataJPA2PostgreSQL/spring-mongodb/mongodbapp/springboot-first-app/src/main/java/com/springboot/first/app/Student.java
@@ -0,0 +1,34 @@
+package com.springboot.first.app;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	
+	
+	public void Student1(String string) {
+		// TODO Auto-generated constructor stub
+	}
+	public void Student2(String string) {
+		// TODO Auto-generated constructor stub
+	}
+	public Student(String string) {
+		// TODO Auto-generated constructor stub
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+	
+
+}
+
